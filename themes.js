97ecb952efe13894aa4d7f15be67f59546b7d013
repositoryrepos <repositoryrepos@gmail.com>
2025-03-18 21:24:57 -0500

    Create themes.js

---
+++ themes.js
@@ -0,0 +1,131 @@
+body {
+    --inputbg: #000000;
+    --inputborder: #000000;
+    --uibg: #000000;
+    --textcolor: #000;
+    --bg: #000000;
+}
+body[theme=custom] {
+    --inputbg: #3c096c;
+    --inputborder: #5a189a;
+    --uibg: #240046;
+    --textcolor: #fff;
+    --bg: #10002b;
+}
+body[theme=main] {
+    --inputbg: #fff;
+    --inputborder: #000;
+    --uibg: #fff;
+    --textcolor: #000;
+     --bg: url("themebgs/IAM.png"), rgb(59, 67, 78);
+    /*
+     https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=light] {
+    --inputbg: #CCCCCC;
+    --inputborder: #BBBBBB;
+    --uibg: #DBDBDB;
+    --textcolor: #000;
+    --bg: #F3F3F3;
+}
+body[theme=dark] {
+    --inputbg: #333333;
+    --inputborder: #444444;
+    --uibg: #242424;
+    --textcolor: #fff;
+    --bg: #0c0c0c;
+}
+body[theme=egamepass] {
+    --inputbg: #4f6ed1;
+    --inputborder: #5586e0;
+    --uibg: #185494;
+    --textcolor: #a7d3ff;
+    --bg: #16416f;
+}
+body[theme=cools1te] {
+    --inputbg: #a134dc;
+    --inputborder: #b153e3;
+    --uibg: #ab28cf;
+    --textcolor: #d3a4fa;
+    --bg: #8e14af;
+}
+body[theme=sunset] {
+    --inputbg: #537294;
+    --inputborder: #30719c;
+    --uibg: #2a7491;
+    --textcolor: #fff;
+    --bg: url("themebgs/sunset_theme.jpg"), rgb(59, 67, 78);
+    /* https://wallhaven.cc/w/jxl3qp */
+    --shadow: 0px 0px 2px #a5dcf5;
+}
+body[theme=mountains] {
+    --inputbg: #d37f7f;
+    --inputborder: #e28a8a;
+    --uibg: #d38493;
+    --textcolor: #fff;
+    --bg: url("themebgs/mountains_theme.jpg"), #bf7483;
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #bba29b;
+}
+body[theme=vaporwave] {
+    --inputbg: #4b2d64;
+    --inputborder: #74568d;
+    --uibg: #51166d;
+    --textcolor: #fff;
+    --bg: url("themebgs/vaporwave_theme.jpg"), #61437a;
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=graduation] {
+    --inputbg: #f080ff;
+    --inputborder: #47afff;
+    --uibg: #9742ff;
+    --textcolor: #fff;
+    --bg: url("themebgs/graduation.png"), #000;
+  /* Full height */
+  height: 100%;
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=late-registration] {
+    --inputbg: #;
+    --inputborder: #;
+    --uibg: #;
+    --textcolor: #fff;
+    --bg: url("themebgs/late.jpg"), #61437a;
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=dropout] {
+    --inputbg: #4b2d64;
+    --inputborder: #74568d;
+    --uibg: #51166d;
+    --textcolor: #fff;
+    --bg: url("themebgs/dropout.jpeg"), #61437a;
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=tylerbirthday] {
+    --inputbg: #3c096c;
+    --inputborder: #5a189a;
+    --uibg: #240046;
+    --textcolor: #fff;
+    --bg: url("themebgs/tylerbday.png"), rgb(59, 67, 78);
+    /* https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
+body[theme=MUSIC] {
+    --inputbg: #fff;
+    --inputborder: #000;
+    --uibg: #fff;
+    --textcolor: #000;
+     --bg: url("themebgs/IAM.png"), rgb(59, 67, 78);
+    /*
+     https://wallhaven.cc/w/l82kpr */
+    --shadow: 0px 0px 4px #000000;
+}
